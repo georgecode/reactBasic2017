f37ee3c808f2bb48d551de5b884f7723a9510261
georgecode <georgeczurlanis@gmail.com>
2017-09-14 02:55:41 -0700

    Add basic state counter increaser

--- src/components/stateBasic/stateBasic.js
+++ src/components/stateBasic/stateBasic.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 
 class StateBasic extends Component {
-	render(){
-		return(
-			<div>
-			<h1>State basic</h1>
-			<div className="break"></div>
-			</div>
-			)
-	}
+   constructor(props) {
+   super(props);
+   this.state = { counter: 0 };
+  }
+
+  render() {
+    return (
+      <div>
+      <h1 className="title">State basic</h1>
+        Counter: {this.state.counter}
+
+        <button 
+          type="button"
+          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+          >
+          Add 1
+        </button>
+
+        <div className="break"></div>
+      </div>
+    );
+  }
 }
 
 
